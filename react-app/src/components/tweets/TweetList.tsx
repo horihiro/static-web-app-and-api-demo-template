104440a0e8c38bbe7828d1db1423d67b414d357f
nohanaga <nohanaga@microsoft.com>
2022-11-04 17:34:33 +0900

    add sentences color

--- react-app/src/components/tweets/TweetList.tsx
+++ react-app/src/components/tweets/TweetList.tsx
@@ -24,148 +24,165 @@ const TweetList = () => {
 
   return (
     <>
-    <div className="row">
-      <div className="col-lg-4 col-12">
-        <div className="card card-info direct-chat direct-chat-info">
-          <div className="card-header">
-            <h3 className="card-title">Positive</h3>
-            <div className="card-tools">
-              <span
-                data-toggle="tooltip"
-                title="3 New Messages"
-                className="badge badge-light"
-              >
-                {positive.length}
-              </span>
-              <button type="button" className="btn btn-tool" data-widget="collapse">
-                <i className="fas fa-minus" />
-              </button>
+      <div className="row">
+        <div className="col-lg-4 col-12">
+          <div className="card card-info direct-chat direct-chat-info">
+            <div className="card-header">
+              <h3 className="card-title">Positive</h3>
+              <div className="card-tools">
+                <span
+                  data-toggle="tooltip"
+                  title="3 New Messages"
+                  className="badge badge-light"
+                >
+                  {positive.length}
+                </span>
+                <button type="button" className="btn btn-tool" data-widget="collapse">
+                  <i className="fas fa-minus" />
+                </button>
+              </div>
+            </div>
+            <div className="card-body">
+              <div className="direct-chat-messages">
+                {positive?.map((item: any) => (
+                  <div className="direct-chat-msg" key={item.TweetId}>
+                    <div className="direct-chat-infos clearfix">
+                      <span className="direct-chat-name float-left">
+                        {item.TweetedBy}
+                      </span>
+                      <span className="direct-chat-timestamp float-right">
+                        {item.CreatedAtIso}
+                      </span>
+                    </div>
+                    <img
+                      className="direct-chat-img"
+                      src="/img/default-profile.png"
+                      alt="message user image" />
+                    <div className="direct-chat-text">
+                      {
+                        (() => {
+                          let sentencelist = [];
+                          for (let i = 0; i < item.sentences.length; i++) {
+                            sentencelist.push(<span className={item.sentences[i].sentiment} key={i}>{item.sentences[i].text}</span>);
+                          }
+                          return (sentencelist)
+                        })()
+                      }
+                    </div>
+                  </div>
+                ))}
+              </div>
+            </div>
+            <div className="card-footer">
             </div>
           </div>
+        </div>
+        <div className="col-lg-4 col-12">
+          <div className="card card-success direct-chat direct-chat-success">
+            <div className="card-header">
+              <h3 className="card-title">Neutral</h3>
+              <div className="card-tools">
+                <span
+                  data-toggle="tooltip"
+                  title="3 New Messages"
+                  className="badge badge-light"
+                >
+                  {neutral.length}
+                </span>
+                <button type="button" className="btn btn-tool" data-widget="collapse">
+                  <i className="fas fa-minus" />
+                </button>
+              </div>
+            </div>
+            <div className="card-body">
+              <div className="direct-chat-messages">
+                {neutral?.map((item: any) => (
+                  <div className="direct-chat-msg" key={item.TweetId}>
+                    <div className="direct-chat-infos clearfix">
+                      <span className="direct-chat-name float-left">
+                        {item.TweetedBy}
+                      </span>
+                      <span className="direct-chat-timestamp float-right">
+                        {item.CreatedAtIso}
+                      </span>
+                    </div>
+                    <img
+                      className="direct-chat-img"
+                      src="/img/default-profile.png"
+                      alt="message user image" />
+                    <div className="direct-chat-text">
+                      {
+                        (() => {
+                          let sentencelist = [];
+                          for (let i = 0; i < item.sentences.length; i++) {
+                            sentencelist.push(<span className={item.sentences[i].sentiment} key={i}>{item.sentences[i].text}</span>);
+                          }
+                          return (sentencelist)
+                        })()
+                      }
+                    </div>
+                  </div>
+                ))}
+              </div>
+            </div>
 
-          <div className="card-body">
-            <div className="direct-chat-messages">
-              {positive?.map((item: any) => (
-                <div className="direct-chat-msg" key={item.TweetId}>
-                  <div className="direct-chat-infos clearfix">
-                    <span className="direct-chat-name float-left">
-                      {item.TweetedBy}
-                    </span>
-                    <span className="direct-chat-timestamp float-right">
-                      {item.CreatedAtIso}
-                    </span>
-                  </div>
-                  <img
-                    className="direct-chat-img"
-                    src="/img/default-profile.png"
-                    alt="message user image" />
-                  <div className="direct-chat-text">
-                    {item.TweetText}
-                  </div>
-                </div>
-              ))}
+            <div className="card-footer">
             </div>
           </div>
-          <div className="card-footer">
+        </div>
+        <div className="col-lg-4 col-12">
+          <div className="card card-danger direct-chat direct-chat-danger">
+            <div className="card-header">
+              <h3 className="card-title">Negative</h3>
+              <div className="card-tools">
+                <span
+                  data-toggle="tooltip"
+                  title="3 New Messages"
+                  className="badge badge-light"
+                >
+                  {negative.length}
+                </span>
+                <button type="button" className="btn btn-tool" data-widget="collapse">
+                  <i className="fas fa-minus" />
+                </button>
+              </div>
+            </div>
+            <div className="card-body">
+              <div className="direct-chat-messages">
+                {negative?.map((item: any) => (
+                  <div className="direct-chat-msg" key={item.TweetId}>
+                    <div className="direct-chat-infos clearfix">
+                      <span className="direct-chat-name float-left">
+                        {item.TweetedBy}
+                      </span>
+                      <span className="direct-chat-timestamp float-right">
+                        {item.CreatedAtIso}
+                      </span>
+                    </div>
+                    <img
+                      className="direct-chat-img"
+                      src="/img/default-profile.png"
+                      alt="message user image" />
+                    <div className="direct-chat-text">
+                      {
+                        (() => {
+                          let sentencelist = [];
+                          for (let i = 0; i < item.sentences.length; i++) {
+                            sentencelist.push(<span className={item.sentences[i].sentiment} key={i}>{item.sentences[i].text}</span>);
+                          }
+                          return (sentencelist)
+                        })()
+                      }
+                    </div>
+                  </div>
+                ))}
+              </div>
+            </div>
+            <div className="card-footer">
+            </div>
           </div>
         </div>
       </div>
-
-      <div className="col-lg-4 col-12">
-        <div className="card card-success direct-chat direct-chat-success">
-          <div className="card-header">
-            <h3 className="card-title">Neutral</h3>
-            <div className="card-tools">
-              <span
-                data-toggle="tooltip"
-                title="3 New Messages"
-                className="badge badge-light"
-              >
-                {neutral.length}
-              </span>
-              <button type="button" className="btn btn-tool" data-widget="collapse">
-                <i className="fas fa-minus" />
-              </button>
-            </div>
-          </div>
-
-          <div className="card-body">
-            <div className="direct-chat-messages">
-              {neutral?.map((item: any) => (
-                <div className="direct-chat-msg" key={item.TweetId}>
-                  <div className="direct-chat-infos clearfix">
-                    <span className="direct-chat-name float-left">
-                    {item.TweetedBy}
-                    </span>
-                    <span className="direct-chat-timestamp float-right">
-                    {item.CreatedAtIso}
-                    </span>
-                  </div>
-                  <img
-                    className="direct-chat-img"
-                    src="/img/default-profile.png"
-                    alt="message user image" />
-                  <div className="direct-chat-text">
-                  {item.TweetText}
-                  </div>
-                </div>
-              ))}
-            </div>
-          </div>
-
-          <div className="card-footer">
-          </div>
-        </div>
-      </div>
-
-
-      <div className="col-lg-4 col-12">
-        <div className="card card-danger direct-chat direct-chat-danger">
-          <div className="card-header">
-            <h3 className="card-title">Negative</h3>
-            <div className="card-tools">
-              <span
-                data-toggle="tooltip"
-                title="3 New Messages"
-                className="badge badge-light"
-              >
-                {negative.length}
-              </span>
-              <button type="button" className="btn btn-tool" data-widget="collapse">
-                <i className="fas fa-minus" />
-              </button>
-            </div>
-          </div>
-
-          <div className="card-body">
-            <div className="direct-chat-messages">
-              {negative?.map((item: any) => (
-                <div className="direct-chat-msg" key={item.TweetId}>
-                  <div className="direct-chat-infos clearfix">
-                    <span className="direct-chat-name float-left">
-                    {item.TweetedBy}
-                    </span>
-                    <span className="direct-chat-timestamp float-right">
-                    {item.CreatedAtIso}
-                    </span>
-                  </div>
-                  <img
-                    className="direct-chat-img"
-                    src="/img/default-profile.png"
-                    alt="message user image" />
-                  <div className="direct-chat-text">
-                  {item.TweetText}
-                  </div>
-                </div>
-              ))}
-            </div>
-          </div>
-
-          <div className="card-footer">
-          </div>
-        </div>
-      </div>
-    </div>
     </>
   );
 };
